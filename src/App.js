21f3f313766a4f44c376e31aa9c5107dc6a4727d
Taras Poiatsyka <tvsxar@MacBook-Air---Taras.local>
2024-11-05 23:26:50 +0200

    first

--- src/App.js
+++ src/App.js
@@ -6,10 +6,55 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then((res) => res.json( ))
+    .then(json => {
+      setUsers(json.data);
+    })
+    .catch(err => {
+      console.warn(err);
+      alert('Receiving user erorr');
+    })
+    .finally(() => setLoading(false));
+  }, []);
+
+  function onChangeSearchValue(event) {
+    setSearchValue(event.target.value);
+  }
+
+  function onClickInvite(id) {
+    if(invites.includes(id)) setInvites(prev => prev.filter(_id => _id !== id))
+    else setInvites(prev => [...prev, id])
+  }
+
+  function onClickSentInvites() {
+    setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {
+        success ? (
+          <Success count={invites.length} />
+        ) : (
+          <Users 
+            onClickSentInvites={onClickSentInvites}
+            invites={invites}
+            onClickInvite={onClickInvite}
+            onChangeSearchValue={onChangeSearchValue}
+            searchValue={searchValue}
+            items={users} 
+            isLoading={isLoading} 
+          />
+        )
+      }
     </div>
   );
 }
